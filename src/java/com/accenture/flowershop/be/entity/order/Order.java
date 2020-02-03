39b521726991edcd34a9bf5bc4cbd384f7bcea45
arturzdan <arturzdan@mail.ru>
2020-02-03 15:59:17 +0300

    add Order.java

--- src/java/com/accenture/flowershop/be/entity/order/Order.java
+++ src/java/com/accenture/flowershop/be/entity/order/Order.java
@@ -1,4 +1,47 @@
 package java.com.accenture.flowershop.be.entity.order;
 
-public interface Order {
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.com.accenture.flowershop.be.entity.flower.Flowers;
+import java.com.accenture.flowershop.be.entity.user.User;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Order {
+    @Id
+    @GeneratedValue()
+    private Long id;
+    private User user;
+    private List<Flowers> flowers;
+    private BigDecimal value;
+
+    public Order(User user, List<Flowers> flowers, BigDecimal value) {
+        this.user = user;
+        this.flowers = flowers;
+        this.value = value;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Flowers> getFlowers() {
+        return flowers;
+    }
+
+    public void setFlowers(List<Flowers> flowers) {
+        this.flowers = flowers;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }

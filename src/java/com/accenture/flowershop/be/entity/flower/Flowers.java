4216fec5907cb94ce0b08a741c7db7f99dbbd42c
arturzdan <arturzdan@mail.ru>
2020-02-03 15:55:41 +0300

    add methods Flowers

--- src/java/com/accenture/flowershop/be/entity/flower/Flowers.java
+++ src/java/com/accenture/flowershop/be/entity/flower/Flowers.java
@@ -2,6 +2,7 @@ package java.com.accenture.flowershop.be.entity.flower;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.math.BigDecimal;
 
 public class Flowers {
     @Id
@@ -9,5 +10,35 @@ public class Flowers {
     private Long id;
     private String name;
     private int count;
-    private double value;
+    private BigDecimal value;
+
+    public Flowers(String name, int count, BigDecimal value) {
+        this.name = name;
+        this.count = count;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }

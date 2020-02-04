b4f18afd746a292817a4adcfb3aa41938972c496
arturzdan <arturzdan@mail.ru>
2020-02-04 13:19:22 +0300

    add class OrderBusiness

---
+++ src/java/com/accenture/flowershop/be/business/order/OrderBusiness.java
@@ -0,0 +1,17 @@
+package java.com.accenture.flowershop.be.business.order;
+
+import java.com.accenture.flowershop.be.entity.order.Order;
+import java.util.List;
+
+public interface OrderBusiness {
+
+    void addOrder(Order order);
+
+    void removeOrder(Order order);
+
+    void updateOrder(Order order);
+
+    Order getOrder(Long id);
+
+    List<Order> getAllOrder();
+}
---
+++ src/java/com/accenture/flowershop/be/business/order/OrderBusinessImpl.java
@@ -0,0 +1,40 @@
+package java.com.accenture.flowershop.be.business.order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.com.accenture.flowershop.be.access.order.OrderAccess;
+import java.com.accenture.flowershop.be.entity.order.Order;
+import java.util.List;
+
+@Service
+public class OrderBusinessImpl implements OrderBusiness {
+
+    @Autowired
+    private OrderAccess orderAccess;
+
+    @Override
+    public void addOrder(Order order) {
+        orderAccess.addOrder(order);
+    }
+
+    @Override
+    public void updateOrder(Order order) {
+        orderAccess.updateOrder(order);
+    }
+
+    @Override
+    public void removeOrder(Order order) {
+        orderAccess.removeOrder(order);
+    }
+
+    @Override
+    public Order getOrder(Long id) {
+        return orderAccess.getOrder(id);
+    }
+
+    @Override
+    public List<Order> getAllOrder() {
+        return null;
+    }
+}

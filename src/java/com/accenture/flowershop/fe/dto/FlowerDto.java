9c0264ff163feaaeb63c391bfa90fbce30b27161
arturzdan <arturzdan@mail.ru>
2020-02-04 13:29:45 +0300

    add class FlowerDto

---
+++ src/java/com/accenture/flowershop/fe/dto/FlowerDto.java
@@ -0,0 +1,51 @@
+package java.com.accenture.flowershop.fe.dto;
+
+import java.com.accenture.flowershop.be.entity.flower.Flowers;
+import java.math.BigDecimal;
+
+public class FlowerDto {
+    private Long id;
+    private String name;
+    private int count;
+    private BigDecimal value;
+
+    FlowerDto adapter(Flowers flower) {
+        this.id = flower.getId();
+        this.name = flower.getName();
+        this.count = flower.getCount();
+        this.value = flower.getValue();
+        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}

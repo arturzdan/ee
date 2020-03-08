8482660200bfffae152a0bbbf65e030de7ada874
arturzdan <arturzdan@mail.ru>
2020-03-08 18:04:04 +0300

    Исправил entity Order

--- src/main/java/com/accenture/flowershop/be/entity/order/Order.java
+++ src/main/java/com/accenture/flowershop/be/entity/order/Order.java
@@ -1,9 +1,12 @@
 package com.accenture.flowershop.be.entity.order;
 
 import javax.persistence.*;
-import com.accenture.flowershop.be.entity.flower.Flowers;
+
+import com.accenture.flowershop.be.entity.cart.CartItem;
 import com.accenture.flowershop.be.entity.user.User;
-import java.math.BigDecimal;
+import com.accenture.flowershop.fe.enums.OrderStatus;
+
+import java.util.Date;
 import java.util.List;
 
 @Entity(name = "Order")
@@ -11,27 +14,27 @@ import java.util.List;
 public class Order {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY, generator = "sequenceId")
+    @SequenceGenerator(name = "sequenceId", sequenceName = "order_sg", allocationSize = 1)
     @Column(name = "ID")
     private Long id;
 
-    //@Column(name = "USER")
-    // todo ManyToOne
-    //private User user;
+    @OneToOne
+    @JoinColumn(name = "ID_USER")
+    private User user;
 
-    //@Column(name = "FLOWERS")
-    //@OneToMany (mappedBy="address", fetch=FetchType.EAGER)
-    // todo ManyToMany
-    //private List<Flowers> flowers;
+    @Column(name = "STATUS")
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
 
-    @Column(name = "VALUE")
-    private BigDecimal value;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "order", cascade = CascadeType.ALL)
+    private List<CartItem> order;
 
-    public Order(User user, List<Flowers> flowers, BigDecimal value) {
-        //this.user = user;
-        //this.flowers = flowers;
-        this.value = value;
-    }
+    @Column(name = "OPEN_DATE")
+    private Date openDate;
+
+    @Column(name = "CLOSE_DATE")
+    private Date closeDate;
 
     public Long getId() {
         return id;
@@ -40,28 +43,44 @@ public class Order {
     public void setId(Long id) {
         this.id = id;
     }
-//
-//    public User getUser() {
-//        return user;
-//    }
-//
-//    public void setUser(User user) {
-//        this.user = user;
-//    }
 
-//    public List<Flowers> getFlowers() {
-//        return flowers;
-//    }
-//
-//    public void setFlowers(List<Flowers> flowers) {
-//        this.flowers = flowers;
-//    }
-
-    public BigDecimal getValue() {
-        return value;
+    public User getUser() {
+        return user;
     }
 
-    public void setValue(BigDecimal value) {
-        this.value = value;
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public List<CartItem> getOrder() {
+        return order;
+    }
+
+    public void setOrder(List<CartItem> order) {
+        this.order = order;
+    }
+
+    public Date getOpenDate() {
+        return openDate;
+    }
+
+    public void setOpenDate(Date openDate) {
+        this.openDate = openDate;
+    }
+
+    public Date getCloseDate() {
+        return closeDate;
+    }
+
+    public void setCloseDate(Date closeDate) {
+        this.closeDate = closeDate;
     }
 }
